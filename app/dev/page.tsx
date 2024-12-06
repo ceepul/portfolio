e7052594703e76ed7c82cc8bde76e91d9533dd1b
Russell Fenton <russell1837@gmail.com>
2024-12-05 23:14:24 -0500

    uninstalled npm tech-stack-icons from project and deleted use from dev page

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -7,22 +7,6 @@ import TextSkills from '@/components/TextSkills';
 import ScrollCTA from '@/components/ScrollCTA';
 
 export default function DevPage() {
-  /* const skills = [
-    { label: 'JavaScript', symbol: <StackIcon name="js" /> },
-    { label: 'TypeScript', symbol: <StackIcon name="typescript" /> },
-    { label: 'React', symbol: <StackIcon name="reactjs" /> },
-    { label: 'NextJS', symbol: <StackIcon name="nextjs2" /> },
-    { label: 'Tailwind', symbol: <StackIcon name="tailwindcss" /> },
-    { label: 'ThreeJS', symbol: <StackIcon name="threejs" /> },
-    { label: 'NodeJS', symbol: <StackIcon name="nodejs" /> },
-    { label: 'AWS', symbol: <StackIcon name="aws" /> },
-    { label: 'Eslint', symbol: <StackIcon name="eslint" /> },
-    { label: 'Fly.io', symbol: <StackIcon name="flyio" /> },
-    { label: 'NPM', symbol: <StackIcon name="npm2" /> },
-    { label: 'OpenAI', symbol: <StackIcon name="openai" /> },
-    { label: 'PostgreSQL', symbol: <StackIcon name="postgresql" /> },
-  ];
- */
   return (
     <div>
       <Header />
@@ -69,7 +53,7 @@ export default function DevPage() {
           <ProjectSection
             id='shopmate ai'
             title='ShopMate AI'
-            description='ShopMate is a lorem ipsum who cares idk one sec ill fix this later okay? Sounds good.'
+            description='ShopMate is a lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum.'
             details={[
               'Pinecone AWS to IDK you know.',
               'Automatically synced product and store data from Shopify GraphQL',
@@ -82,7 +66,7 @@ export default function DevPage() {
           <ProjectSection
             id='3d virtual try-on'
             title='3D Virtual Try-On'
-            description='ShopMate is a lorem ipsum who cares idk one sec ill fix this later okay? Sounds good.'
+            description='ShopMate is a lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum.'
             details={[
               'Pinecone AWS to IDK you know.',
               'Automatically synced product and store data from Shopify GraphQL',
@@ -96,7 +80,7 @@ export default function DevPage() {
           <ProjectSection
             id='solana pay'
             title='Solana Pay'
-            description='ShopMate is a lorem ipsum who cares idk one sec ill fix this later okay? Sounds good.'
+            description='ShopMate is a lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum.'
             details={[
               'Pinecone AWS to IDK you know.',
               'Automatically synced product and store data from Shopify GraphQL',
