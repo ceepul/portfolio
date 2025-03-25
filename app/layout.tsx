ec902c6343fc5eb3938ffb3e3b9c9de27030a099
Russell Fenton <russell1837@gmail.com>
2025-03-25 09:39:11 -0400

    Updated NextJS version to address security vulnerability in NextJS V15.0.3. Added Vercel speed insights to app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const josefinSans = Josefin_Sans({
   subsets: ['latin'],
